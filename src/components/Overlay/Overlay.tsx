02262257d96ab7445b847a288c0a4de06b3d7436
Albert Tran <albertq.tran1@gmail.com>
2024-10-07 10:27:05 -0400

    added default sprite

--- src/components/Overlay/Overlay.tsx
+++ src/components/Overlay/Overlay.tsx
@@ -1,21 +1,21 @@
-import { IconUser } from "@tabler/icons-react"
-import clsx from "clsx"
-import { useContext } from "react";
-import { SpriteData, ThemeContext } from "../../App";
+// import { IconUser } from "@tabler/icons-react"
+// import clsx from "clsx"
+// import { useContext } from "react";
+import { SpriteData } from "../../App";
 import { UploadButton } from "./UploadButton";
 
 
 export const Overlay = (props: {addSprite : (data: SpriteData) => void}) => {
-    const { theme } = useContext(ThemeContext);
+    // const { theme } = useContext(ThemeContext);
 
     return (
-    <div className="absolute border w-full h-full z-10">
-        <div className="absolute border left-0 bottom-0">
+    <div className="absolute w-full h-full z-10">
+        <div className="absolute left-0 bottom-0">
             <div className="flex flex-col m-4 gap-4">
-                <IconUser size={40} className={clsx(
+                {/* <IconUser size={40} className={clsx(
                                                     theme === "dark" && "text-neutral-700 hover:text-neutral-100",
                                                     theme === "light" && "text-neutral-400 hover:text-neutral-900",
-                                                    "transition-colors cursor-pointer")}/>
+                                                    "transition-colors cursor-pointer")}/> */}
                 <UploadButton addSprite={props.addSprite}/>
             </div>
         </div>
--- src/components/Overlay/UploadModal/UploadModal.tsx
+++ src/components/Overlay/UploadModal/UploadModal.tsx
@@ -6,6 +6,7 @@ import { v4 } from 'uuid';
 import MultiUploadScreen from './MultiUploadScreen';
 import ControlsScreen from './ControlsScreen';
 import { SpriteData } from '../../../App';
+import * as DefaultSprite from "../../../assets/DefaultSprite/index"
 
 type ModalState = "UPLOAD" | "SPRITESHEET" | "MULTIIMAGE" | "CONTROLS"
 
@@ -24,6 +25,7 @@ const UploadModal = (props: {addSprite : (data: SpriteData) => void}) => {
     ])
     
     const onDropSpriteSheet = useCallback((acceptedFiles : File[]) => {
+        console.log(acceptedFiles)
         const multiDrop = acceptedFiles.length >= 2;
         setImageFiles(acceptedFiles.map((file) => {
             return {data: file, url: URL.createObjectURL(file)}
@@ -34,6 +36,45 @@ const UploadModal = (props: {addSprite : (data: SpriteData) => void}) => {
             setModalState("SPRITESHEET")
         }
     }, [])
+
+
+    const UploadDefaults = async () => {
+        let fArr : {data: File, url: string}[] = []
+        const defaultImageFiles = [
+            {file: DefaultSprite.idle1, name: "idle1.jpeg"},
+            {file: DefaultSprite.idle2, name: "idle2.jpeg"},
+            {file: DefaultSprite.idle3, name: "idle3.jpeg"},
+            {file: DefaultSprite.idle4, name: "idle4.jpeg"},
+            {file: DefaultSprite.idle5, name: "idle5.jpeg"},
+            {file: DefaultSprite.jump1, name: "jump1.jpeg"},
+            {file: DefaultSprite.jump2, name: "jump2.jpeg"},
+            {file: DefaultSprite.jump3, name: "jump3.jpeg"},
+            {file: DefaultSprite.jump4, name: "jump4.jpeg"},
+            {file: DefaultSprite.jump5, name: "jump5.jpeg"},
+            {file: DefaultSprite.run1, name: "run1.jpeg"},
+            {file: DefaultSprite.run2, name: "run2.jpeg"},
+            {file: DefaultSprite.run3, name: "run3.jpeg"},
+            {file: DefaultSprite.run4, name: "run4.jpeg"},
+            {file: DefaultSprite.run5, name: "run5.jpeg"},
+            {file: DefaultSprite.run6, name: "run6.jpeg"},
+            {file: DefaultSprite.run7, name: "run7.jpeg"},
+        ]
+
+
+
+        for (const data of defaultImageFiles) {
+            let res = await fetch(data.file)
+            let blob = await res.blob()
+            const contentType = res.headers.get('content-type')!
+
+            let file =  new File([blob],data.name, {type: contentType})
+            console.log(file)
+            fArr.push({data: file,url: URL.createObjectURL(file)});
+        }
+
+        setImageFiles(fArr)
+        setModalState("MULTIIMAGE");
+    }
 
     const {getRootProps : rootSpriteSheetDrop, getInputProps : inputSpriteSheetDrop, isDragActive : spriteSheetDragActive} = useDropzone({onDrop: onDropSpriteSheet, accept:{"image/png": [".png"],"image/jpg": [".jpg"], "image/jpeg": [".jpeg"]}})
 
@@ -50,11 +91,23 @@ const UploadModal = (props: {addSprite : (data: SpriteData) => void}) => {
                                             transition-colors cursor-pointer rounded border-dashed`}
                                 style={{borderColor: spriteSheetDragActive ? "blue" : 'inherit'}}>
                             <input {...inputSpriteSheetDrop({accept:"image/*"})} />
-                            <div className="flex flex-col gal-2 items-center">
+                            <div className="flex flex-col gap-2 items-center">
                                 <IconUpload size={64} stroke={1}/>
                                 <div className="text-base mt-4 max-w-[75%] text-center">
                                     Drop a single sprite sheet / multiple images here or click to upload.
                                 </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className={`h-full flex justify-center items-center h-full 
+                                            border text-neutral-400 text-2xl hover:bg-neutral-200 
+                                            hover:text-neutral-600 hover:border-neutral-600 
+                                            transition-colors cursor-pointer rounded border-dashed`}
+                            onClick={(UploadDefaults)}>
+                            <div className="flex flex-col gap-2 items-center">
+                                <div className="text-base mt-4 max-w-[75%] text-center">
+                                    Just want to try the app? Click here to use the default sprite package
                                 </div>
                             </div>
                         </div>
